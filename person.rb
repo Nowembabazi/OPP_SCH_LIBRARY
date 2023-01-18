6469de3c75ea96183f1d57ee1ca24902dcfbb091
Nickson Nowembabazi <109085430+Nowembabazi@users.noreply.github.com>
2023-01-18 10:38:36 +0300

    add person.rb

--- person.rb
+++ person.rb
@@ -1,12 +1,17 @@
-class Person
+require_relative('./nameable')
+require_relative('./rental')
+
+class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
   attr_reader :id
 
   def of_age?
@@ -17,5 +22,13 @@ class Person
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
+  end
+
+  def add_rental(date, book)
+    Rental.new(date, self, book)
   end
 end
